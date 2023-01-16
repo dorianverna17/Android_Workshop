cb4fa7d8ce34e4f31654909d9bfb1af8082b7d55
Dorian Verna <dorian.verna@stud.acs.upb.ro>
2023-01-16 18:15:00 +0200

    Finished workshop 2 tutorial

--- Android2/app/src/main/java/ro/pub/acs/myapplication/MainActivity.kt
+++ Android2/app/src/main/java/ro/pub/acs/myapplication/MainActivity.kt
@@ -1,11 +1,79 @@
 package ro.pub.acs.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import ro.pub.acs.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Dorian")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+
+//        findViewById<Button>(R.id.done_button).setOnClickListener { button ->
+//            addNick(button)
+//        }
+
+        binding.doneButton.setOnClickListener { addNick(it) }
+
+//        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+//            changeNick(it)
+//        }
+
+        binding.nicknameText.setOnClickListener { changeNick(it) }
+    }
+
+    private fun addNick(view: View) {
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nickTextView = findViewById<TextView>(R.id.nickname_text)
+
+////        val editText = binding.nicknameEdit
+////        val nickTextView = binding.nicknameText
+
+        lateinit var editText: EditText
+        lateinit var nickTextView: TextView
+
+        binding.apply {
+            editText = nicknameEdit
+            nickTextView = nicknameText
+        }
+
+//        nickTextView.text = editText.text
+
+        binding.myName?.nickname = editText.text.toString()
+        binding.invalidateAll()
+
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickTextView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun changeNick(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val button = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        button.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        editText.requestFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
---
+++ Android2/app/src/main/java/ro/pub/acs/myapplication/MyName.kt
@@ -0,0 +1,3 @@
+package ro.pub.acs.myapplication
+
+data class MyName(var name: String = "", var nickname: String = "")
